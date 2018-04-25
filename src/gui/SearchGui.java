306976a90d9bf84be2437f72ce350c4fdb85c17b
wendyltan <youlinai233@gmail.com>
2018-04-25 19:22:31 +0800

    修改界面功能

--- src/gui/SearchGui.java
+++ src/gui/SearchGui.java
@@ -3,13 +3,16 @@ package gui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
-import sun.plugin.javascript.navig.Anchor;
 
 public class SearchGui extends Application {
+
+    private static Stage stage;
+    public static Stage getGuiStage(){
+        return stage;
+    }
+
 
     public static void main(String[] args) {
         launch(args);
@@ -21,6 +24,7 @@ public class SearchGui extends Application {
             // 这里的root从FXML文件中加载进行初始化，这里FXMLLoader类用于加载FXML文件
             FlowPane root =  (FlowPane)FXMLLoader.load(getClass().getResource("SearchGui.fxml"));
             Scene scene = new Scene(root);
+            stage = primaryStage;
             primaryStage.setScene(scene);
             primaryStage.setTitle("SearchDir");
             primaryStage.setResizable(false);
--- src/gui/SearchGuiHandler.java
+++ src/gui/SearchGuiHandler.java
@@ -1,14 +1,17 @@
 package gui;
 
 import code.*;
-import com.sun.org.apache.bcel.internal.generic.IF_ACMPEQ;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.stage.FileChooser;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,43 +20,106 @@ public class SearchGuiHandler {
     @FXML
     private Button searchBtn;
     @FXML
-    private TextArea oldLogTextarea;
+    private TextArea leftLogTextarea;
     @FXML
-    private TextArea newLogTextarea;
+    private TextArea rightLogTextarea;
     @FXML
-    private MenuItem compare_menu;
+    private CheckMenuItem compare_menu;
     @FXML
-    private MenuItem log_menu;
+    private CheckMenuItem log_menu;
     @FXML
     private MenuItem help_menu;
+
     @FXML
-    private Button saveLogBtn;
+    private Button compareLogBtn;
     @FXML
     private Button readLogBtn;
     @FXML
-    private ChoiceBox chooseSort;
+    private ChoiceBox<String> chooseSort;
+    @FXML
+    private ChoiceBox<String> chooseShow;
     @FXML
     private TextField pathText;
     @FXML
     private Label hintLb;
 
 
+
     private List<SDirectory> dirs = new ArrayList<>();
     private SearchPanel panel = new SearchPanel();
-    //下次改进使其可以选择
-    private Sort sort = new AlphaSort();
-
     private String path;
     private ModeFactory mode;
+    private TextArea current;
+    private Sort sort;
+    //显示在左窗口还是右窗口
+    private static final int LEFT_WINDOW = 0 ;
+    private static final int RIGHT_WINDOW = 1;
 
-    
+    @FXML
+    public void initialize() {
+
+        ObservableList<String> availableSorts = FXCollections.observableArrayList("AlphaSort", "TypeSort","DateSort");
+        chooseSort.setItems(availableSorts);
+        chooseSort.setValue(availableSorts.get(0));
+        chooseSort.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                sort =null;
+                String type = newValue;
+                if (type == "AlphaSort") {
+                    sort = new AlphaSort();
+                    hintLb.setText("Using AlphaSort!");
+                }else if(type.equals("TypeSort")){
+                    sort = new TypeSort();
+                    hintLb.setText("Using TypeSort!");
+                }else if (type.equals("DateSort")){
+                    sort = new DateSort();
+                    hintLb.setText("Using DateSort!");
+                }
+
+            }
+        });
+
+        ObservableList<String> availableWindow = FXCollections.observableArrayList("left","right");
+        chooseShow.setItems(availableWindow);
+        chooseShow.setValue(availableWindow.get(0));
+        chooseShow.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                String type = newValue;
+                if (type.equals("left") ) {
+                    current = chooseLog(LEFT_WINDOW);
+                    hintLb.setText("Using Left Window!");
+                }else if(type.equals("right")){
+                    current = chooseLog(RIGHT_WINDOW);
+                    hintLb.setText("Using Right Window!");
+                }
+            }
+        });
+
+        leftLogTextarea.setEditable(false);
+        rightLogTextarea.setEditable(false);
+
+
+        //initialize as default
+        sort = new AlphaSort();
+        current = leftLogTextarea;
+        compareLogBtn.setDisable(true);
+        log_menu.setSelected(true);
+    }
+
+
+    /**
+     * handle search event.Enter directory path and
+     * save log file
+     * then read from log file and show on left_window
+     * @param event
+     */
 
     @FXML
     protected void handleSearch(ActionEvent event) {
-        //清空上一次的路径
-        if (!oldLogTextarea.getText().isEmpty()){
-            oldLogTextarea.clear();
-        }
+
+        current.clear();
 
         //假设现在输入的目录格式都是正确的
         path = pathText.getText();
@@ -68,8 +134,10 @@ public class SearchGuiHandler {
             }
         }
         if (!dirs.isEmpty()) {
+            //using current sort method
             sortDir(dirs);
             long before = System.currentTimeMillis();
+
             //save log
             mode = ModeFactory.getMode("Log");
             LogMode log = (LogMode) mode;
@@ -78,6 +146,7 @@ public class SearchGuiHandler {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
             //read log and show
             BufferedInputStream inputStream = mode.getLogData(ModeFactory.ORI_LOG);
             try{
@@ -87,7 +156,7 @@ public class SearchGuiHandler {
                 while ((bytesRead = inputStream.read(buff)) != -1 ) {
                     // 显示行号
                     String chunk = new String(buff, 0, bytesRead);
-                    oldLogTextarea.appendText(chunk);
+                    current.appendText(chunk);
                 }
 
                 inputStream.close();
@@ -98,15 +167,17 @@ public class SearchGuiHandler {
 
             long after = System.currentTimeMillis();
             hintLb.setText("Consume time: "+ String.valueOf((after-before)/ 1000D) + "s");
-            saveLogBtn.setDisable(false);
 
         }
     }
 
-    public boolean sortDir(List<SDirectory> dirs){
+    /**
+     * sort directory using selected sort method
+     * @param dirs
+     * @return
+     */
+    private boolean sortDir(List<SDirectory> dirs){
         if (!dirs.isEmpty()) {
-            //sort and show
-            Sort sort = new AlphaSort();
             for (SDirectory dir : dirs) {
                 dir.setSortCriteria(sort);
                 dir.getSortCriteria().sort(FileListManager.getFileList(dir.getFilePath()));
@@ -117,52 +188,115 @@ public class SearchGuiHandler {
 
     }
 
-    public void handleLogSave(ActionEvent actionEvent) {
-
-        if (sortDir(dirs)){
-            mode = ModeFactory.getMode("Log");
-            LogMode log = (LogMode) mode;
-            try {
-                log.saveLogFile(path,dirs);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            hintLb.setText("Log save success!");
-            readLogBtn.setDisable(false);
-        }else{
-            hintLb.setText("You can't save log before u search!");
-            saveLogBtn.setDisable(true);
-            readLogBtn.setDisable(true);
+    /**
+     * choose textarea according to window code
+     * @param windowId
+     * @return
+     */
+    private TextArea chooseLog(int windowId){
+        switch (windowId){
+            case LEFT_WINDOW:
+                return leftLogTextarea;
+            case RIGHT_WINDOW:
+                return rightLogTextarea;
         }
-
+        return null;
     }
 
+    /**
+     * read log from file.
+     * @param actionEvent
+     */
     public void handleLogRead(ActionEvent actionEvent) {
-        oldLogTextarea.clear();
-        if (mode==null){
-            hintLb.setText("You can't read before u save!");
-            readLogBtn.setDisable(true);
-        }else{
-            BufferedInputStream inputStream = mode.getLogData(ModeFactory.ORI_LOG);
-            try{
-
+        current.clear();
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Choose log file");
+        File logFile = fileChooser.showOpenDialog(SearchGui.getGuiStage());
+        if (logFile!=null){
+            try {
+                BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(logFile));
                 int bytesRead = 0;
                 byte[] buff = new byte[4096];
                 long before = System.currentTimeMillis();
                 while ((bytesRead = inputStream.read(buff)) != -1 ) {
                     // 显示行号
                     String chunk = new String(buff, 0, bytesRead);
-                    oldLogTextarea.appendText(chunk);
+                    current.appendText(chunk);
                 }
                 long after = System.currentTimeMillis();
-
                 hintLb.setText("Log read success! Consume time: "+ String.valueOf((after-before)/ 1000D) + "s");
                 inputStream.close();
-            }
-            catch (IOException e) {
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
                 e.printStackTrace();
             }
+
+        }else{
+            hintLb.setText("Didn't choose any file!");
         }
 
+
+    }
+
+    public void handleCompare(ActionEvent actionEvent) {
+
+    }
+
+    public void onModeCompare(ActionEvent actionEvent) {
+        compareLogBtn.setDisable(false);
+        compare_menu.setSelected(true);
+        log_menu.setSelected(false);
+        hintLb.setText("You are in compare mode!");
+    }
+
+    public void onModeLog(ActionEvent actionEvent) {
+        compareLogBtn.setDisable(true);
+        log_menu.setSelected(true);
+        compare_menu.setSelected(false);
+        hintLb.setText("You are in log mode!");
+    }
+
+    public void onHelp(ActionEvent actionEvent) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("关于");
+        alert.setHeaderText("目录搜索差异比较程序v1.0");
+        alert.setContentText("当前版本处于测试状态，请阅读readme");
+        Label label = new Label("readme.md :");
+
+        File file = new File("README.md");
+        TextArea textArea = new TextArea();
+
+
+        try {
+            BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(file));
+            int bytesRead = 0;
+            byte[] buff = new byte[4096];
+            while ((bytesRead = inputStream.read(buff)) != -1 ) {
+                // 显示行号
+                String chunk = new String(buff, 0, bytesRead);
+                textArea.appendText(chunk);
+            }
+            inputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        textArea.setEditable(false);
+        textArea.setWrapText(false);
+
+        textArea.setMaxWidth(Double.MAX_VALUE);
+        textArea.setMaxHeight(Double.MAX_VALUE);
+        GridPane.setVgrow(textArea, Priority.ALWAYS);
+        GridPane.setHgrow(textArea, Priority.ALWAYS);
+
+        GridPane expContent = new GridPane();
+        expContent.setMaxWidth(Double.MAX_VALUE);
+        expContent.add(label, 0, 0);
+        expContent.add(textArea, 0, 1);
+
+        alert.getDialogPane().setExpandableContent(expContent);
+        alert.showAndWait();
     }
 }
